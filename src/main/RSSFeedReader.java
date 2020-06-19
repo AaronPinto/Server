ad16c8559f53617096afad6a9701045ffed04c8c
Aaron Pinto <aaronp110@gmail.com>
2020-06-19 03:06:33 -0400

    redid the XML parsing and the filtering

--- src/main/RSSFeedReader.java
+++ src/main/RSSFeedReader.java
@@ -5,7 +5,12 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.services.gmail.Gmail;
 import com.google.common.base.Charsets;
 import main.util.GoogleMail;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
@@ -13,6 +18,8 @@ import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class RSSFeedReader {
     static void start() {
@@ -30,28 +37,46 @@ public class RSSFeedReader {
                 while (true) {
                     try {
                         var in = new BufferedReader(new InputStreamReader(new URL("https://blogs.windows.com/feed/").openStream()));
+                        StringBuilder sb = new StringBuilder();
                         String line;
 
+                        outer:
                         while ((line = in.readLine()) != null) {
-                            int linkStartIndex, linkEndIndex = 0;
-                            linkStartIndex = line.indexOf("<link>", linkEndIndex);
+                            if (line.contains("<channel>")) {
+                                sb.append(line);
 
-                            if (linkStartIndex >= 0) {
-                                linkEndIndex = line.indexOf("</link>", linkStartIndex);
-                                String link = line.substring(linkStartIndex + "<link>".length(), linkEndIndex);
+                                while ((line = in.readLine()) != null) {
+                                    sb.append(line);
 
-                                if (link.contains("announcing-windows-10-insider-preview-build") && link
-                                        .contains("windowsexperience") && prevLinks.stream().noneMatch(s -> s.equals(link))) {
-                                    String title = link.substring(link.indexOf("announcing"), link.length() - 1);
-                                    System.out.println(title + " " + LocalDateTime.now());
-
-                                    pw.println(link);
-                                    pw.flush();
-
-                                    prevLinks = Files.readAllLines(Paths.get(linksPath), Charsets.UTF_8);
-
-                                    GoogleMail.sendMessage(gmail, "me", GoogleMail.createEmail(recipient, recipient, title, link));
+                                    if (line.contains("</channel>")) {
+                                        break outer;
+                                    }
                                 }
+                            }
+                        }
+
+                        var docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                        var nList = docBuilder.parse(new InputSource(new StringReader(sb.toString()))).getElementsByTagName("item");
+
+                        for (int i = 0; i < nList.getLength(); i++) {
+                            var element = (Element) nList.item(i);
+
+                            String title = element.getElementsByTagName("title").item(0).getTextContent();
+                            String link = element.getElementsByTagName("link").item(0).getTextContent();
+                            NodeList temp = element.getElementsByTagName("category");
+                            var categories = IntStream.range(0, temp.getLength()).mapToObj(temp::item).map(Node::getTextContent)
+                                    .collect(Collectors.toList());
+
+                            if (categories.stream().anyMatch(s -> s.contains("Windows Insider Program")) && prevLinks.stream()
+                                    .noneMatch(s -> s.equals(link))) {
+                                System.out.println(title + " " + LocalDateTime.now());
+
+                                pw.println(link);
+                                pw.flush();
+
+                                prevLinks = Files.readAllLines(Paths.get(linksPath), Charsets.UTF_8);
+
+                                GoogleMail.sendMessage(gmail, "me", GoogleMail.createEmail(recipient, recipient, title, link));
                             }
                         }
 

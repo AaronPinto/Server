ade6c94e314bb1705ba2bb8c37b23a7b4d2419f4
Aaron <10177767+AaronPinto@users.noreply.github.com>
2021-07-04 03:12:56 -0400

    updated webserver to support HTTP/2 and added basic website

--- src/webserver.js
+++ src/webserver.js
@@ -1,15 +1,45 @@
-const http = require('http'), fs = require('fs'), mime = require('mime');
+const fs = require("fs");
+const http = require("http");
+const http2 = require("http2");
 
-http.createServer((req, res) => {
-    let path = decodeURI(req.url);
-    fs.readFile("A&W" + path, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.writeHead(404, {'Content-Type': 'text/html'});
-        } else {
-            res.writeHead(200, {'Content-Type': mime.getType(path)});
-            res.write(data);
-        }
-        res.end();
+const port = 443;
+const key = fs.readFileSync("localhost-privkey.pem");
+const cert = fs.readFileSync("localhost-cert.pem");
+const data = fs.readFileSync("index.html");
+
+const server = http2.createSecureServer({key, cert, allowHTTP1: true}, (req, res) => {
+    const {httpVersion, method, socket, url} = req;
+    const {remoteAddress} = socket;
+    const {socket: {alpnProtocol}} = req.httpVersion === "2.0" ? req.stream.session : req;
+
+    res.on("finish", () => {
+        console.log(`${getLocalDateTime()} Received ${method} request from ${remoteAddress} using HTTP ${httpVersion} and ${alpnProtocol} for URL ${url}`);
     });
+
+    res.writeHead(200, {"content-type": "text/html; charset=utf-8"});
+    res.end(data);
+});
+server.on("error", err => console.error(err));
+
+server.listen(port, () => {
+    console.log(`${getLocalDateTime()} Server running at https://127.0.0.1:${port}/`);
+});
+
+// Redirect requests on port 80 to 443
+http.createServer((req, res) => {
+    const {httpVersion, method, socket, url} = req;
+    const {remoteAddress} = socket;
+
+    res.on("finish", () => {
+        console.log(`${getLocalDateTime()} Received ${method} request from ${remoteAddress} using HTTP ${httpVersion} for URL ${url}`);
+    });
+
+    res.writeHead(301, {"Location": "https://" + req.headers.host + req.url});
+    res.end();
 }).listen(80);
+
+// https://stackoverflow.com/a/51643788/6713362
+function getLocalDateTime() {
+    const currDate = new Date();
+    return new Date(currDate - currDate.getTimezoneOffset() * 60000).toISOString().slice(0, 23);
+}
